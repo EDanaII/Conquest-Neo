09bfffed307b8ce8df688abde38964e0573b38dd
adm401bga <145081438+adm401bga@users.noreply.github.com>
2025-09-04 15:37:45 -0700

    Reworked map; interfaces to their own directory

--- Conquest.CLI/MapRenderer.cs
+++ Conquest.CLI/MapRenderer.cs
@@ -8,33 +8,33 @@ public sealed class MapRenderer : IMapRenderer {
         var w = state.Config.Width;
         var h = state.Config.Height;
 
-        // Top X axis (labels shown 1..W, internal indices 0..W-1)
-        io.Write("   ");
-        for (int x = 0; x < w; x++)
-            io.Write(((x + 1) % 10 == 0) ? "0" : ((x + 1) % 10).ToString( ));
-        io.WriteLine( );
-
         // Rows (internal 0..h-1, print labels as 1..h)
         for (int y = h - 1; y >= 0; y--) {
-            io.Write((y + 1).ToString( ).PadLeft(2) + " ");
+            io.Write(y == 0 ? " 1|" : ((y + 1) % 5 == 0 ? (y + 1).ToString( ).PadLeft(2) + "|" : "  |"));
+
             for (int x = 0; x < w; x++) {
                 var cell = state.Board[x, y];
                 // show star first letter if explored by player; '?' if not; '.' if no star
                 if (cell.StarRef is Star s) {
                     var seen = !state.FogEnabled || s.ExploredBy[1]; // team 1 = player
-                    io.Write(seen ? s.Name[0].ToString( ) : "?");
+                    io.Write(seen ? $" {s.Name[0].ToString( )} " : " ? ");
                 }
                 else {
-                    io.Write(".");
+                    io.Write(" . ");
                 }
             }
-            io.WriteLine(" " + (y + 1).ToString( ).PadLeft(2));
+            io.WriteLine("|");
         }
 
         // Bottom X axis (labels shown 1..W)
         io.Write("   ");
         for (int x = 0; x < w; x++)
-            io.Write(((x + 1) % 10 == 0) ? "0" : ((x + 1) % 10).ToString( ));
+            io.Write("---");
+        io.WriteLine("");
+
+        io.Write("   ");
+        for (int x = 0; x < w; x++)
+            io.Write(x == 0 ? " 1 " : ((x + 1) % 5 == 0 ? (x + 1).ToString( ).PadLeft(2) + " " : "   "));
         io.WriteLine( );
     }
 }
